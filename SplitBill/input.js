0265c277136e17ed1e9787480a8e11fb754a3426
charanpratap <vasacp193691@gmail.com>
2023-06-21 16:20:24 +0530

    some updatations

--- SplitBill/input.js
+++ SplitBill/input.js
@@ -2,19 +2,29 @@
 
 function splitcal(){
     var inputNum = document.getElementById('numAmount').value;
-    // if(inputNum <= 0 || inputNum < 1){
-    //     document.getElementById('numAmount').style.outlineColor='red';
-    //     // document.getElementById('numPersons').style.outlineColor='red';
-    // }
-    // let errorMsg = (function(){
-    //     if(inputNum != 0)
-    //     return document.getElementById('numAmount').style.outlineColor = 'red';
-    //    })();
-    
+   
     var noPersons = document.getElementById('numPersons').value;
     var splitedAmount =(inputNum / noPersons).toFixed(2);
     document.getElementById('splitAmount').value = splitedAmount;
+
+    if(inputNum <= 0){
+        document.getElementById('errormsg1').innerHTML = `Amount should be more than 0`;
+    }
+    if(noPersons <= 1){
+        document.getElementById('errormsg2').innerHTML = `persons should be more than 1`;
+    }else {
+        document.getElementById('errormsg2').innerHTML = ``;
+    }
+}
+function empty(){
+document.getElementById('numAmount').value = "";
+document.getElementById('numPersons').value = ""; 
+document.getElementById('errormsg1').innerHTML = "";
+document.getElementById('errormsg2').innerHTML = "";
 }
 
-
+const printDate = (()=>{
+    let date = new Date();
+    return document.getElementById('date').innerHTML = date;
+})();
 
--- sum_of_random_nos/script.js
+++ sum_of_random_nos/script.js
@@ -1,24 +1,25 @@
 
 
-function randomValue1(){
-    var value1 = Math.random() * 150 ;
-    var value2 = Math.random() * 100 ;
+function randomValue(){
+    var value1 = Math.random() * 500 ;
+    var value2 = Math.random() * 400 ;
     var num1 = parseInt(value1.toFixed());
     var num2 = parseInt(value2.toFixed());
     document.getElementById('random1').value = num1;
     document.getElementById('random2').value = num2;
-
-    document.getElementById('sum').value = `${num1+num2}`;
+    document.getElementById('sum').value = "";
+       
 }
+function addValue(){
+    num1 = parseInt(document.getElementById('random1').value);
+    num2 = parseInt(document.getElementById('random2').value);
 
-// console.log(num1);
-   
-// console.log(num1); 
-
+    document.getElementById('sum').value = `${num1 + num2}`;
+}
 
 let message = (function(){
    let date = new Date();
-   return document.getElementById('sum1').innerHTML = date;
+   return document.getElementById('date').innerHTML = date;
   })();
 
 
