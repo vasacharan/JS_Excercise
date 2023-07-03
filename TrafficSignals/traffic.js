412cab00aa4a9a4a88c434cb5decdc987daf5c4f
charanpratap <vasacp193691@gmail.com>
2023-07-03 15:15:27 +0530

    Traffic signals Programe addes

---
+++ TrafficSignals/traffic.js
@@ -0,0 +1,33 @@
+// const infiniteTraffic = setInterval(TrafficLights,3000);
+
+function TrafficLights(){
+        const myRedTime = setInterval(redLight, 3000);
+            function redLight(){
+                document.querySelector('#boxa').classList.remove('box3');
+                document.querySelector('#boxa').classList.add('box1');
+                document.querySelector('.list-red').classList.remove('green','yellow');
+                document.querySelector('.list-red').classList.add('red');
+                
+        }
+        const myYellowTime = setInterval(yellowLight, 6000);
+            function yellowLight(){
+                document.querySelector('#boxa').classList.add('box2');
+                document.querySelector('#boxa').classList.remove('box1');
+                document.querySelector('.list-red').classList.remove('red','green');
+                document.querySelector('.list-yellow').classList.add('yellow');
+            
+        }
+        const myGreenTime = setInterval(greenLight, 9000);
+            function greenLight(){
+                document.querySelector('#boxa').classList.remove('box2');
+                document.querySelector('#boxa').classList.add('box3');
+                document.querySelector('.list-red').classList.remove('yellow','red');
+                document.querySelector('.list-green').classList.add('green');
+                
+        }
+}
+function stopTimer(){
+    clearInterval(myRedTime);
+    // clearInterval(myYellowTime);
+    // clearInterval(myGreenTime);
+}
